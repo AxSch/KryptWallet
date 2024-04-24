cfd5e723863d6480a5d2209f2e4292f463be35d1
AxSch <alex_schuneman@outlook.com>
2024-04-24 22:59:31 +0100

    Refactor contract to separate addresses of partners

--- contracts/Partnership.sol
+++ contracts/Partnership.sol
@@ -6,14 +6,16 @@ import "hardhat/console.sol";
 contract Partnership {
     string private deploymentMessage = "Contract is deployed";
     uint256 private partnerAmount = 2;
-    address[] public addresses;
+    address public partner1;
+    address public partner2;
 
-    constructor(address[] memory _addresses) {
+    constructor(address _partner1, address _partner2) {
         require(
-            _addresses.length == partnerAmount,
-            "You cannot have more than 2 partners"
+            _partner1 != address(0) && _partner2 != address(0),
+            "Invalid partner address"
         );
-        addresses = _addresses;
+        partner1 = _partner1;
+        partner2 = _partner2;
 
         console.log(deploymentMessage);
     }
